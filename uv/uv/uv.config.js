f7b4f13860676f72782af44ea7677b380014501f
Xelenu <158429867+Xelenu@users.noreply.github.com>
2025-06-24 17:43:59 -0700

    Update uv.config.js

--- uv/uv/uv.config.js
+++ uv/uv/uv.config.js
@@ -1,13 +1,11 @@
-// This file overwrites the stock UV config.js
-
 self.__uv$config = {
-  prefix: "/uv/service/",
-  bare: "/bare/",
+  prefix: "/ww/loading/",
+  bare: "https://prox-ww.learnstats.xyz",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
-  handler: "/uv/uv.handler.js",
-  client: "/uv/uv.client.js",
-  bundle: "/uv/uv.bundle.js",
-  config: "/uv/uv.config.js",
-  sw: "/uv/uv.sw.js",
+  handler: "/ww/uv.handler.js",
+  client: "/ww/uv.client.js",
+  bundle: "/ww/uv.bundle.js",
+  config: "/ww/uv.config.js",
+  sw: "/ww/uv.sw.js",
 };
