2113371a9f4287ca209d3b7a8079639ca8cbfa90
Xelenu <158429867+Xelenu@users.noreply.github.com>
2025-06-29 16:43:20 -0700

    Update uv.config.js

--- cra/zim/uv.config.js
+++ cra/zim/uv.config.js
@@ -1,12 +1,10 @@
-// This file overwrites the stock UV config.js
-
 self.__uv$config = {
   prefix: "/cra/zim/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
   handler: "/cra/uv.handler.js",
-  client: "/cra/uv.client.js",
-  bundle: "/cra/uv.bundle.js",
-  config: "/cra/uv.config.js",
-  sw: "/cra/uv.sw.js",
+  client:  "/cra/uv.client.js",
+  bundle:  "/cra/uv.bundle.js",
+  config:  "/cra/uv.config.js",
+  sw:      "/cra/uv.sw.js",
 };
