142e9c4a9a4148a283ebb1d9a236d1bd2d16a9c5
Xelenu <158429867+Xelenu@users.noreply.github.com>
2025-06-29 22:14:07 -0700

    Update Chocolate-Wolfenstein-3D.js

--- core/gam-scripts/wolfenstein/Chocolate-Wolfenstein-3D.js
+++ core/gam-scripts/wolfenstein/Chocolate-Wolfenstein-3D.js
@@ -15,7 +15,7 @@ Module.expectedDataFileDownloads++;
             throw "using preloaded data can only be done on a web page or in a web worker"
         }
         var PACKAGE_NAME = "Chocolate-Wolfenstein-3D.data";
-        var REMOTE_PACKAGE_BASE = "Chocolate-Wolfenstein-3D.data";
+        var REMOTE_PACKAGE_BASE = "/core/gam-scripts/wolfenstein/Chocolate-Wolfenstein-3D.data";
         if (typeof Module["locateFilePackage"] === "function" && !Module["locateFile"]) {
             Module["locateFile"] = Module["locateFilePackage"];
             err("warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)")
