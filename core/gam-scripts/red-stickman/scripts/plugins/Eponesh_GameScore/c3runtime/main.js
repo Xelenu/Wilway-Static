40a412ec9943f4d6d2b306ffd2ad725dfbf37f37
Xelenu <158429867+Xelenu@users.noreply.github.com>
2025-07-22 15:22:59 -0700

    Update main.js

--- core/gam-scripts/red-stickman/scripts/plugins/Eponesh_GameScore/c3runtime/main.js
+++ core/gam-scripts/red-stickman/scripts/plugins/Eponesh_GameScore/c3runtime/main.js
@@ -1,6 +1,6 @@
-import "/core/gam-scripts/red-stickman/scripts/plugins/Eponish_GameScore/c3runtime/plugin.js";
-import "/core/gam-scripts/red-stickman/scripts/plugins/Eponish_GameScore/c3runtime/type.js";
-import "/core/gam-scripts/red-stickman/scripts/plugins/Eponish_GameScore/c3runtime/instance.js";
-import "/core/gam-scripts/red-stickman/scripts/plugins/Eponish_GameScore/c3runtime/conditions.js";
-import "/core/gam-scripts/red-stickman/scripts/plugins/Eponish_GameScore/c3runtime/actions.js";
-import "/core/gam-scripts/red-stickman/scripts/plugins/Eponish_GameScore/c3runtime/expressions.js";
+import "/core/gam-scripts/red-stickman/scripts/plugins/Eponesh_GameScore/c3runtime/plugin.js";
+import "/core/gam-scripts/red-stickman/scripts/plugins/Eponesh_GameScore/c3runtime/type.js";
+import "/core/gam-scripts/red-stickman/scripts/plugins/Eponesh_GameScore/c3runtime/instance.js";
+import "/core/gam-scripts/red-stickman/scripts/plugins/Eponesh_GameScore/c3runtime/conditions.js";
+import "/core/gam-scripts/red-stickman/scripts/plugins/Eponesh_GameScore/c3runtime/actions.js";
+import "/core/gam-scripts/red-stickman/scripts/plugins/Eponesh_GameScore/c3runtime/expressions.js";
