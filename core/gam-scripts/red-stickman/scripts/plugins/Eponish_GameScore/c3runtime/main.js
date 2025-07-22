2acc71ef8de72b7b96bf04e3e1f3a4d0ce7ad5f1
Xelenu <158429867+Xelenu@users.noreply.github.com>
2025-07-21 19:38:10 -0700

    Update main.js

--- core/gam-scripts/red-stickman/scripts/plugins/Eponish_GameScore/c3runtime/main.js
+++ core/gam-scripts/red-stickman/scripts/plugins/Eponish_GameScore/c3runtime/main.js
@@ -1,6 +1,6 @@
-import "./plugin.js";
-import "./type.js";
-import "./instance.js";
-import "./conditions.js";
-import "./actions.js";
-import "./expressions.js";
+import "/core/gam-scripts/red-stickman/scripts/plugins/Eponish_GameScore/c3runtime/plugin.js";
+import "/core/gam-scripts/red-stickman/scripts/plugins/Eponish_GameScore/c3runtime/type.js";
+import "/core/gam-scripts/red-stickman/scripts/plugins/Eponish_GameScore/c3runtime/instance.js";
+import "/core/gam-scripts/red-stickman/scripts/plugins/Eponish_GameScore/c3runtime/conditions.js";
+import "/core/gam-scripts/red-stickman/scripts/plugins/Eponish_GameScore/c3runtime/actions.js";
+import "/core/gam-scripts/red-stickman/scripts/plugins/Eponish_GameScore/c3runtime/expressions.js";
